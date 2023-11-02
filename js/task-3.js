b24522e5da47dc689acf1dd8701e904e4d870225
trubchaninov Andriy 1-8 <a.qwerty0444@gmail.com>
2023-11-02 23:17:14 +0200

    add tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,17 @@
-//Ширина елемента
+//Перевірка спаму
 'use strict'
-function getElementWidth(content, padding, border) {
-    let contentNum = parseFloat(content, 1);
-    let paddingNum = parseFloat(padding, 1);
-    let borderNum = parseFloat(border, 1);
-    const borderBox = contentNum + paddingNum * 2 + borderNum * 2;
-    return borderBox
+function checkForSpam(message) {
+    let hasLowerMessage = message.toLowerCase();
+    if (hasLowerMessage.includes("spam") || hasLowerMessage.includes("sale")) {
+        return true;
+    } else {
+        return false;
+    }
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
